f5049d9a1d7588c4145f7299122e2ad655169e55
Noah Hoffman <stacknoah99@gmail.com>
2024-05-02 19:59:12 -0600

    made the basic styles dynamic for fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import data from "../data/data.json";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleDropDown() {}
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <table
+      style={{ border: "1px solid black", width: "100%", textAlign: "left" }}
+    >
+      <thead>
+        <tr>
+          <th className="table-headings">
+            Name
+            <ArrowDropDownIcon
+              style={{ display: "inline-block", verticalAlign: "middle" }}
+            />
+          </th>
+          <th className="table-headings">
+            Type
+            <ArrowDropDownIcon
+              style={{ display: "inline-block", verticalAlign: "middle" }}
+            />
+          </th>
+          <th className="table-headings">
+            Created By
+            <ArrowDropDownIcon
+              style={{ display: "inline-block", verticalAlign: "middle" }}
+            />
+          </th>
+          <th className="table-headings">
+            Modified By
+            <ArrowDropDownIcon
+              style={{ display: "inline-block", verticalAlign: "middle" }}
+            />
+          </th>
+          <th className="table-headings">
+            Last Modified
+            <ArrowDropDownIcon
+              style={{ display: "inline-block", verticalAlign: "middle" }}
+            />
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.Reports.map((report, index) => (
+          <tr key={index} className="table-row">
+            <td className="table-el">
+              <a>{report.name}</a>
+            </td>
+            <td className="table-el">{report.type}</td>
+            <td className="table-el">{report.created_by}</td>
+            <td className="table-el">{report.modified_by}</td>
+            <td className="table-el">{report.last_modified}</td>
+            <td className="table-el">
+              <button
+                className="btn"
+                style={{
+                  display: "inline-block",
+                  verticalAlign: "middle",
+                  color: "blue",
+                }}
+                onClick={handleDropDown}
+              >
+                Actions
+                <KeyboardArrowUpIcon
+                  style={{ display: "inline-block", verticalAlign: "middle" }}
+                />
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
-export default App
+export default App;
